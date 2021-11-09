685c3b2463ab544fe84b4c9436bf20fc63e34df9
OhmRee <13455401+ohmree@users.noreply.github.com>
2021-11-09 23:04:27 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <QCoreApplication>
+#include <QtJsonSerializer/jsonserializer.h>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    printf("hello xmake\n");
+    return a.exec();
+}
